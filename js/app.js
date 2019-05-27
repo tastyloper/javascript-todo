c700102ae5bf93715aa1e939f5aa2566d121f99d
tastyloper <tastyloper@gmail.com>
2019-05-27 18:32:02 +0900

    make mvc class

--- js/app.js
+++ js/app.js
@@ -1,92 +1,102 @@
 (function () {
+  class Todo {
+    constructor (todos = []) {
+      this.todos = todos;
+      this.$inputTodo = document.querySelector('.input-todo');
+      this.$todos = document.querySelector('.todos');
+      this.$completeAll = document.querySelector('.custom-checkbox');
+      this.$completedTodos = document.querySelector('.completed-todos');
+      this.$activeTodos = document.querySelector('.active-todos');
+      this.$clearBtn = document.querySelector('.btn');
+
+      this.$todos.addEventListener('click', this.removeEvent.bind(this));
+      this.$inputTodo.addEventListener('keyup', this.addEvent.bind(this));
+      this.$todos.addEventListener('change', this.checkEvent.bind(this));
+      this.$completeAll.addEventListener('click', this.completeAllEvent.bind(this));
+      this.$clearBtn.addEventListener('click', this.allClearEvent.bind(this));
+      this.htmlRendering();
+    }
+
+    htmlRendering() {
+      let html = '';
+      this.todos.forEach(({id, content, completed}) => {
+        html += `<li id="${id}" class="todo-item"><input class="custom-checkbox" type="checkbox" id="ck-${id}"${completed ? ' checked' : ''}><label for="ck-${id}">${content}</label><i class="remove-todo far fa-times-circle"></i></li>`
+      });
+      this.$todos.innerHTML = html;
+  
+      this.$completedTodos.innerHTML = this.countCompletedTodos();
+      this.$activeTodos.innerHTML = this.countNotCompletedTodos();
+    }
+  
+    removeTodo(id) {
+      this.todos = this.todos.filter(todo => todo.id !== +id);
+    }
+  
+    gerenateId() {
+      return this.todos.length ? Math.max(...this.todos.map(todo => todo.id)) + 1 : 1;
+    }
+  
+    addTodo(content) {
+      this.todos = [{id: this.gerenateId(), content, completed: false}, ...this.todos];
+    }
+  
+    changeCheck(itemId) {
+      this.todos = this.todos.map(todo => todo.id === itemId ? Object.assign({}, todo, {completed: !todo.completed}) : todo);
+    }
+  
+    toggleCompletedAll(checked) {
+      this.todos = this.todos.map(val => Object.assign({}, val, {completed: checked}));
+    }
+  
+    countCompletedTodos() {
+      return this.todos.filter(val => val.completed).length;
+    }
+  
+    countNotCompletedTodos() {
+      return this.todos.filter(val => !val.completed).length;
+    }
+  
+    clearAllDel() {
+      this.todos = this.todos.filter(val => !val.completed);
+    }
+
+    removeEvent(e) {
+      if (!e.target.classList.contains('remove-todo')) return;
+      this.removeTodo(e.target.parentNode.id);
+      this.htmlRendering();
+    }
+
+    addEvent(e) {
+      const val = this.$inputTodo.value.trim();
+      if (val === '' || e.keyCode !== 13) return;
+  
+      this.addTodo(val);
+      this.$inputTodo.value = '';
+      this.$inputTodo.focus();
+      this.htmlRendering();
+    }
+
+    checkEvent(e) {
+      this.changeCheck(+e.target.parentNode.id);
+      this.htmlRendering();
+    }
+
+    completeAllEvent(e) {
+      this.toggleCompletedAll(e.target.checked);
+      this.htmlRendering();
+    }
+
+    allClearEvent(e) {
+      this.clearAllDel();
+      this.htmlRendering();
+    }
+  }
+
   let todos = [
     { id: 1, content: 'HTML', completed: true },
     { id: 2, content: 'CSS', completed: true },
     { id: 3, content: 'Javascript', completed: false }
   ];
-  const $inputTodo = document.querySelector('.input-todo');
-  const $todos = document.querySelector('.todos');
-  const $completeAll = document.querySelector('.custom-checkbox');
-  const $completedTodos = document.querySelector('.completed-todos');
-  const $activeTodos = document.querySelector('.active-todos');
-  const $clearBtn = document.querySelector('.btn');
-
-  
-  function htmlRendering() {
-    let html = '';
-    todos.forEach(({id, content, completed}) => {
-      html += `<li id="${id}" class="todo-item"><input class="custom-checkbox" type="checkbox" id="ck-${id}"${completed ? ' checked' : ''}><label for="ck-${id}">${content}</label><i class="remove-todo far fa-times-circle"></i></li>`
-    });
-    $todos.innerHTML = html;
-
-    $completedTodos.innerHTML = countCompletedTodos();
-    $activeTodos.innerHTML = countNotCompletedTodos();
-  }
-
-  function removeTodo(id) {
-    todos = todos.filter(todo => todo.id !== +id);
-  }
-
-  function gerenateId() {
-    return todos.length ? Math.max(...todos.map(todo => todo.id)) + 1 : 1;
-  }
-
-  function addTodo(content) {
-    todos = [{id: gerenateId(), content, completed: false}, ...todos];
-  }
-
-  function changeCheck(itemId) {
-    todos = todos.map(todo => todo.id === itemId ? Object.assign({}, todo, {completed: !todo.completed}) : todo);
-  }
-
-  function toggleCompletedAll(checked) {
-    todos = todos.map((val) => Object.assign({}, val, {completed: checked}));
-  }
-
-  function countCompletedTodos() {
-    return todos.filter((val) => val.completed).length;
-  }
-
-  function countNotCompletedTodos() {
-    return todos.filter((val) => !val.completed).length;
-  }
-
-  function clearAllDel() {
-    todos = todos.filter((val) => !val.completed);
-  }
-
-
-  $todos.addEventListener('click', (e) => {
-    if (!e.target.classList.contains('remove-todo')) return;
-    removeTodo(e.target.parentNode.id);
-    htmlRendering();
-  });
-
-  $inputTodo.addEventListener('keyup', (e) => {
-    const val = $inputTodo.value.trim();
-    if (val === '' || e.keyCode !== 13) return;
-
-    addTodo(val);
-    $inputTodo.value = '';
-    $inputTodo.focus();
-    htmlRendering();
-  });
-
-  $todos.addEventListener('change', (e) => {
-    changeCheck(+e.target.parentNode.id);
-    htmlRendering();
-  });
-
-  $completeAll.addEventListener('click', (e) => {
-    toggleCompletedAll(e.target.checked);
-    htmlRendering();
-  });
-
-  $clearBtn.addEventListener('click', (e) => {
-    clearAllDel();
-    htmlRendering();
-  });
-
-  htmlRendering();
+  new Todo(todos);
 
 }());
